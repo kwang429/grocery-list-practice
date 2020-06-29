23c457ff6175821af7fb90762ab1a4b608d233d4
Kat <katherine.wang429@gmail.com>
2020-06-29 14:08:20 -0700

    Init server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+// middleware
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// create express app
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use the index.html in dist folder
+app.use('/', express.static(path.join(__dirname, '../client/dist/')));
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Jotting down groceries on port ${port}`));
